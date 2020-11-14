ad437301807290763374f803979aa8e52b7dec06
Majd Hafiri <majd.hafiri@stud.th-deg.de>
2020-11-14 16:30:22 +0100

    Update applications/prime/cryptocore_app_Alice_C1.c

--- applications/prime/cryptocore_app_Alice_C1.c
+++ applications/prime/cryptocore_app_Alice_C1.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char get_b_value(){
 
-    char b[128];
+char b[128];
+char * get_b_value(void){
+
+    // char b[128];
     FILE *fptr;
 
     if ((fptr = fopen("/home/data_user/b.txt","r")) == NULL){
@@ -18,13 +20,13 @@ char get_b_value(){
     // printf("Value of b=%s", b);
     fclose(fptr);
 
-    return *b;
+    return b;
 
 }
 
 int main()
 {
-    printf(get_b_value());
+    printf("%s",get_b_value());
   
    return 0;
 }
