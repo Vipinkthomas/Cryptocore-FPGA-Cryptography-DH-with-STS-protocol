38b60f5e96a74aa180fbc7a539ab302052ee1817
Majd Hafiri <majd.hafiri@stud.th-deg.de>
2020-11-14 16:11:50 +0100

    Update applications/prime/cryptocore_app_Alice_C1.c

--- applications/prime/cryptocore_app_Alice_C1.c
+++ applications/prime/cryptocore_app_Alice_C1.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char get_b_value(){
+
+    char b[128];
+    FILE *fptr;
+
+    if ((fptr = fopen("/home/data_user/b.txt","r")) == NULL){
+        printf("Error! opening file");
+
+        // Program exits if the file pointer returns NULL.
+        exit(1);
+    }
+
+    fscanf(fptr,"%s", b);
+
+    // printf("Value of b=%s", b);
+    fclose(fptr);
+
+    return b;
+
+}
+
 int main()
 {
-   char num[1000];
-   FILE *fptr;
-
-   if ((fptr = fopen("/home/data_user/b.txt","r")) == NULL){
-       printf("Error! opening file");
-
-       // Program exits if the file pointer returns NULL.
-       exit(1);
-   }
-
-   fscanf(fptr,"%s", num);
-
-   printf("Value of n=%s", num);
-   fclose(fptr); 
+    printf(get_b_value());
   
    return 0;
 }
