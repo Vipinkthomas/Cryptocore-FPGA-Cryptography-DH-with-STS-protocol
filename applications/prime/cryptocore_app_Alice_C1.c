402d0b19d1570035136669d35c2e45eb600bc3ea
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2020-11-19 09:01:54 +0100

    Update applications/prime/cryptocore_app_Alice_C1.c

--- applications/prime/cryptocore_app_Alice_C1.c
+++ applications/prime/cryptocore_app_Alice_C1.c
@@ -272,28 +272,27 @@ void Fileread(FILE *f,__u32 *output)
 	char n_string[] = "";
 	fscanf(f,"%s", n_string);
 
-    __u32 *output_n1, *temp_n;
+    __u32 *temp_n;
     char *tok_n;
     int elements_n = 0;
     int len_n = 1 + strlen(n_string) / 2;            // estimate max num of elements
-    output_n1 = malloc(len_n* sizeof(*output_n1));
+    output = malloc(len_n* sizeof(*output));
 
-    if (output_n1 == NULL)
+    if (output == NULL)
         exit(-1);                               // memory alloc error
 
     tok_n = strtok(n_string, ",");                  // parse the string
     while (tok_n != NULL) {
         if (elements_n >= len_n)
             exit(-2);                           // error in length assumption
-        if (1 != sscanf(tok_n, "%x", output_n1 + elements_n))
+        if (1 != sscanf(tok_n, "%x", output+ elements_n))
             exit(-3);                           // error in string format
         elements_n++;
         tok_n = strtok(NULL, ",");
     }
 
-    temp_n = realloc(output_n1, elements_n * sizeof(*output_n1)); // resize the array
+    temp_n = realloc(output, elements_n * sizeof(*output)); // resize the array
     if (temp_n == NULL)
         exit(-4);                               // error in reallocating memory
-    output_n1= temp_n;
-	output=output_n1;
+    output= temp_n;
 }
