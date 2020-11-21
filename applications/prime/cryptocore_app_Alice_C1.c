7af0248c39472adb7e662c446aec87e364d38ef0
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2020-11-21 13:41:51 +0100

    Update applications/prime/cryptocore_app_Alice_C1.c

--- applications/prime/cryptocore_app_Alice_C1.c
+++ applications/prime/cryptocore_app_Alice_C1.c
@@ -14,7 +14,7 @@ __u32 *output;
 /* Prototypes for functions used to access physical memory addresses */
 int open_physical (int);
 void close_physical (int);
-void Fileread(FILE *,__u32 *);
+void Fileread(FILE *);
 
 int main(void)
 {	
