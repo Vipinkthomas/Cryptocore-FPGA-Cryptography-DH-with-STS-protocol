606301acaba0a13adcc139acb1aa3f2cdca3c5c0
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2020-12-16 16:40:37 +0100

    Update connections/first.sh

--- connections/first.sh
+++ connections/first.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # This is a comment!
 echo Hello World         # This is a comment, too!
+openssl genrsa -out privkey.pem 2048
+openssl rsa -in privkey.pem -outform PEM -pubout -out pubkey.pem
