f657badea8b180bf323d133a84d7c1ed509510cf
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2021-01-27 18:31:48 +0100

    Update Main/createEncSig.sh

--- Main/createEncSig.sh
+++ Main/createEncSig.sh
@@ -1,3 +1,3 @@
 openssl dgst -sha256 -sign /home/bob/bob.key -out /home/bob/signatureBob.sha256 /home/bob/cAliceBob.txt
 openssl enc -base64 -in /home/bob/signatureBob.sha256 -out /home/bob/signatureBob.base64
-openssl enc -salt -aes-256-cbc -in /home/bob/signatureBob.base64 -kfile secret.txt -out signatureBob.enc
+openssl enc -salt -aes-256-cbc -in /home/bob/signatureBob.base64 -kfile /home/bob/secret.txt -out /home/bob/signatureBob.enc
