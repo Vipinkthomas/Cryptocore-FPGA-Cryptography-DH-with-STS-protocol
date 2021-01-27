9e38a549009e7e2ed0b26cca8d14919e70d22fe9
Majd Hafiri <majd.hafiri@stud.th-deg.de>
2021-01-27 23:07:46 +0100

    Update Main/main.py

--- Main/main.py
+++ Main/main.py
@@ -230,14 +230,18 @@ if __name__ == '__main__':
             # subprocess.call('cd /home/bob/stoesd_ii_2020-21/Main ; ./createCertificate.sh', shell=True)
         
         print("Congrats, Now you can talk to each other securely")
+        while userMenuInput != 'bye':
+            userMenuInput=input()
+            thread5.start()
+            thread6.start()
+
+        # thread1.join()
+        # thread2.join()
+        # thread3.join()
+        # thread4.join()
         
-        thread1.join()
-        thread2.join()
-        thread3.join()
-        thread4.join()
-        
-        thread5.start()
-        thread6.start()
+        # thread5.start()
+        # thread6.start()
 
     # thread2.join()
 
