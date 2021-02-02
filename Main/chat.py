b775e61f5d270494fef754765dbc59fe5dd09409
Majd Hafiri <majd.hafiri@stud.th-deg.de>
2021-02-02 21:01:49 +0100

    Update chat.py

--- Main/chat.py
+++ Main/chat.py
@@ -13,66 +13,25 @@ def connect(s):
 
     while True:
         received = s.recv(4096)
-        if received == '':
-            file = open("/home/bob/cAlice.txt", "wb")
+        if received:
+            file = open("/home/bob/aliceMsg.txt", "wb")
             RecvData = s.recv(4096)
             file.write(RecvData)
             file.close()
-            print("cAlice has been received")
-            pass
 
-           
     
 
 ## This function will encode the message from user input and send it and also transfer files across
 ## chanel
 def sendMsg(s):
 
-    
-    s_msg = userMenuInput.encode('utf-8')
-    if s_msg == '':
-        pass
-
     ## if string message pubk received, bob will send a message "pubk" along with the pubk.pem file to alice
     ## which will later use it to decrypt the signature
-    elif s_msg.decode() == '6':
-        s.send(b'bobSignature')
-        file = open("/home/bob/signatureBob.enc", "rb")
-        SendData = file.read(4096)
-        s.send(SendData)
-        file.close()
-
-    ## if string message cert received, bob will send a message "cert" along with the Certificate.crt file to alice
-    ## which will use it to verify the sender
-    elif s_msg.decode() == '7':
-        s.send(b'bobCertificate')
-        file = open("/home/bob/bob.crt", "rb")
-        SendData = file.read(4096)
-        s.send(SendData)
-        file.close()
-
-    ## if string message encMsg received, bob will send a message "encSig" along with the encrpyted sign.sha256.base64 file to alice 
-    ## this file will be decrypted and check if it matches with the hashing value of the original message
-    elif s_msg.decode() == '8':
-
-        s.send(b'cBob')
-        file = open("/home/bob/cBob.txt", "rb")
-        SendData = file.read(4096)
-        s.send(SendData)
-        file.close()
-
-    elif s_msg.decode() == 'Msg':
-        ## if string message encMsg received, bob will send a message "Msg" along with the encrpyted encMsgBob.enc file to alice 
-        ## this file will be decrypted and check if it matches with the hashing value of the original message
-        s.send(b'Msg')
+    if userInput == '2':
         file = open("/home/bob/encMsgBob.enc", "rb")
         SendData = file.read(4096)
         s.send(SendData)
         file.close()
-
-
-    else:
-        s.sendall(s_msg)
 
 
 
@@ -104,7 +63,20 @@ if __name__ == '__main__':
     thread1.start()
     thread2.start()
 
-    userMenuInput= ''
+    while True:
+
+        userInput = input("Enter the message: ")
+        file = open("/home/bob/bobMsg.txt", "wb")
+            file.write(userInput)
+            file.close()
+        
+        subprocess.call('openssl enc -salt -aes-256-cbc -in /home/bob/bobMsg.txt -kfile /home/bob/secret.txt -out /home/bob/encMsgBob.enc', shell=True)
+
+        userInput = input("Enter 2 to send the encrypted message to ALICE: ")
+
+        
+        
+
             
 
 
