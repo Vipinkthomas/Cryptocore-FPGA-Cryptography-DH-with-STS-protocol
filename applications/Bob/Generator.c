04c891cf863d5a58d97e8350ad3afd03d9345abe
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2021-02-11 19:15:05 +0100

    Update applications/Bob/Generator.c

--- applications/Bob/Generator.c
+++ applications/Bob/Generator.c
@@ -11,31 +11,31 @@
 
 #include "../../include/cryptocore_ioctl_header.h"
 
-__u32 *output;// For reading the content of the file to a pointer
-
 /* Prototypes for functions used to access physical memory addresses */
-int open_physical (int);
-void close_physical (int);
-void Fileread(FILE *);
-bool isGen(ModExp_params_t)
+
+int open_physical (int); // For reading the content of the file to a pointer
+void close_physical (int);// For reading the content of the file to a pointer
+
+
+void Fileread(FILE *);// For reading the content of the file to a pointer
 
 int main(void)
 {	
 	
 
-	int dd = -1;
-	int ret_val;
-	__u32 trng_val = 0;
-	__u32 i = 0;
+	int dd = -1; // For reading the content of the file to a pointer
+	int ret_val; // For reading the content of the file to a pointer
+	__u32 trng_val = 0; // For reading the content of the file to a pointer
+	__u32 i = 0; // For reading the content of the file to a pointer
 
-	double seconds;
-	struct timespec tstart={0,0}, tend={0,0};
+	double seconds; // For reading the content of the file to a pointer
+	struct timespec tstart={0,0}, tend={0,0}; // For reading the content of the file to a pointer
 
 	// Random number creator for Ephermeral DH - exponent component
-	if ((dd = open_physical (dd)) == -1)
+	if ((dd = open_physical (dd)) == -1) // For reading the content of the file to a pointer
       return (-1);
 
-    // Stop TRNG and clear FIFO
+	// Stop TRNG and clear FIFO
 	trng_val = 0x00000010;
 	ret_val = ioctl(dd, IOCTL_SET_TRNG_CMD, &trng_val);
 	if(ret_val != 0) {
@@ -75,89 +75,61 @@ int main(void)
 	usleep(10); // For reading the content of the file to a pointer
 
 	// For reading the content of the file to a pointer
+	struct TRNG_params TRNG_4096_test = { 4096, 
+	{  } };
 
-    ModExp_params_t ModExp_4096_test = { 4096,
-	1,
-	0,
-	{  },
-	{  },
-	{  },
-	{  },
-	};
+
+	clock_gettime(CLOCK_MONOTONIC, &tstart); // For reading the content of the file to a pointer
+
+	// Read TRNG FIRO
+	TRNG_4096_test.rand[0]=0x0; // For reading the content of the file to a pointer
+	TRNG_4096_test.rand[1]=0xffffffff; // For reading the content of the file to a pointer
 
 	// For reading the content of the file to a pointer
-    FILE *fp1 = fopen("/home/data_user/n.txt", "r");
-    if (fp1 == NULL) {
-        fprintf(stderr, "Can't read file");
-        return 0;
-    }
-
-    Fileread(fp1);
-	
-	i = 0;
-	while (i < ModExp_4096_test.prec/32) {
-		
-		ModExp_4096_test.n[i] = output[i];
-		i++;
-		
-	}	
-
-    ModExp_4096_test.b[0]=0x0; // For reading the content of the file to a pointer
-	ModExp_4096_test.b[1]=0xffffffff; // For reading the content of the file to a pointer
-    while(true){
-	for(i=2; i<ModExp_4096_test.prec/32; i++){
+	for(i=2; i<TRNG_4096_test.prec/32; i++){
 		ret_val = ioctl(dd, IOCTL_READ_TRNG_FIFO, &trng_val);
 		if(ret_val == 0) {
-			ModExp_4096_test.b[i] = trng_val;
+			TRNG_4096_test.rand[i] = trng_val;
 		} else{
 			printf("Error occured\n");
 		}
 	}
 
-    if(isGen(ModExp_4096_test))
-    break;
-
-    }
-
+	clock_gettime(CLOCK_MONOTONIC, &tend); //Writing e to e.txt
 
 	
-	FILE *f_write = fopen("/home/data_user/gen.txt", "w");
-    
-    char hexString [128]= "";
-      for(i=0 ; i< ModExp_4096_test.prec/32; i++){
-        sprintf(hexString, "%08x,", ModExp_4096_test.c[i]);
-        fprintf(f_write,"%s",hexString);
-    }
-	printf("CBob = ModExp(R,R,E,B,P): 0x");
-	for(i=0; i<ModExp_4096_test.prec/32; i++){
-		printf("%08x", ModExp_4096_test.c[i]);
-	}
-	printf("\n\n");
-
-	FILE *f_write = fopen("/home/bob/gen.txt", "w");//Writing e to e.txt
+	FILE *f_write = fopen("/home/bob/b.txt", "w");//Writing e to e.txt
     
     char hexString [128]= "";//Writing e to e.txt
 
-	//Writing gen to gen.txt
-    for(i=0 ; i< ModExp_4096_test.prec/32; i++){
-        sprintf(hexString, "%08x,", ModExp_4096_test.c[i]);
+	//Writing e to e.txt
+    for(i=0 ; i< TRNG_4096_test.prec/32; i++){
+        sprintf(hexString, "%08x,", TRNG_4096_test.rand[i]);
         fprintf(f_write,"%s",hexString);
     }
 
 	
-	printf("gen has been created");
+	printf("Exponent has been created");
 	printf("\n\n");
 
+	//Writing e to e.txt
+	seconds = ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec);
+	if (seconds*1000000.0 > 1000.0)
+		printf("Reading 4096 random bits took about %.5f ms\n", seconds*1000.0);
+	else 
+		printf("Reading 4096 random bits took about %.5f us\n", seconds*1000000.0);	
 
-	close_physical (dd);   // close /dev/cryptocore
-    //file close and free
-    fclose(fp1);
-	fclose(f_write);
+
+	//Writing e to e.txt
+	close_physical (dd);
 	return 0;
+
 }
 
+
+//Writing e to e.txt
 int open_physical (int dd)
-{
+{		
    if (dd == -1)
       if ((dd = open( "/dev/cryptocore", (O_RDWR | O_SYNC))) == -1)
       {
@@ -167,46 +139,8 @@ int open_physical (int dd)
    return dd;
 }
 
-
+//Writing e to e.txt
 void close_physical (int dd)
 {
    close (dd);
 }
-
-// function
-
-void Fileread(FILE *fp)
-{	char n_string[4096]="";
-	__u32 *temp_n;
-	fscanf(fp,"%s", n_string);
-    char *tok_n;
-    int elements_n = 0;
-    int len_n = 1 + strlen(n_string) / 2;            // estimate max num of elements
-    output = malloc(len_n* sizeof(*output));
-
-    if (output == NULL)
-        exit(-1);                               // memory alloc error 
-    tok_n = strtok(n_string, ",");   
-    while (tok_n != NULL) {
-        if (elements_n >= len_n)
-            exit(-2);                           // error in length assumption
-        if (1 != sscanf(tok_n, "%x", output + elements_n))
-            exit(-3);                           // error in string format
-        elements_n++;
-        tok_n = strtok(NULL, ",");
-    }
-
-    temp_n = realloc(output, elements_n * sizeof(*output)); // resize the array
-    if (temp_n == NULL)
-        exit(-4);                               // error in reallocating memory
-    output = temp_n;
-}
-
-bool isGen(ModExp_4096_test)
-{	
-    ret_val = ioctl(dd, IOCTL_MWMAC_MODEXP, &ModExp_4096_test);
-    if(ret_val != 0) {
-		printf("Error occured\n");
-	}
-}
-
