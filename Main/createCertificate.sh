f1d63ddba02d935406c07b72baf7739651dbb8c6
Vipin Thomas <vipin.thomas@stud.th-deg.de>
2021-02-11 19:36:31 +0100

    Revert "Merge branch 'US13_alice' into 'master'"
    This reverts merge request !11

--- Main/createCertificate.sh
+++
@@ -1,3 +0,0 @@
-openssl genrsa -out /home/alice/alice.key 2048
-openssl req -sha256 -new -key /home/alice/alice.key -out /home/alice/alice.csr
-openssl x509 -sha256 -req -in /home/alice/alice.csr -CA  /home/data_user/rootCA/ca.crt -CAkey  /home/data_user/rootCA/ca.key -CAcreateserial -out /home/alice/alice.crt -days 7300
--- Main/createEncSig.sh
+++
@@ -1,3 +0,0 @@
-openssl dgst -sha256 -sign /home/alice/alice.key -out /home/alice/signatureAlice.sha256 /home/alice/cAliceBob.txt
-openssl enc -base64 -in /home/alice/signatureAlice.sha256 -out /home/alice/signatureAlice.base64
-openssl enc -salt -aes-256-cbc -in /home/alice/signatureAlice.base64 -kfile /home/alice/secret.txt -out /home/alice/signatureAlice.enc
